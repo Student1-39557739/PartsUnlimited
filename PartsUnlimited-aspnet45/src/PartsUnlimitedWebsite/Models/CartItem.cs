c3334c5a5176aecbc9921c38a9f8c353f511084e
Akis <akontakis@cepal.gr>
2024-04-09 11:47:11 +0300

    My first commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -15,7 +15,7 @@ namespace PartsUnlimited.Models
 
         [DataType(DataType.DateTime)]
         public DateTime DateCreated { get; set; }
-
+// change
         public virtual Product Product { get; set; }
     }
 }
